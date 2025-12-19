1c606596f46cf61782eb38d4612327bc2614d79f
Leonid Burshtein <LeonidBurshtein@yandex.ru>
2025-12-19 23:01:52 +0300

    добавлены вспомогательные аннотации

---
+++ src/test/java/com/github/leonidstein/utils/annotations/TestType.java
@@ -0,0 +1,18 @@
+package com.github.leonidstein.utils.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface TestType {
+
+    Type value();
+
+    enum Type {
+        POSITIVE,
+        NEGATIVE
+    }
+}
